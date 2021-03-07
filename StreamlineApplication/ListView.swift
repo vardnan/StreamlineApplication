566a8caf5558302a2a5e6834ce3e94fa5c6176ab
Vardnan <46565659+vardnan@users.noreply.github.com>
2021-03-07 20:45:42 +0100

    add assets, IB outlets and actions

---
+++ StreamlineApplication/ListView.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  StreamlineApplication
+//
+//  Created by Vardnan on 07/03/2021.
+//
+
+import UIKit
+
+class ListView: UIViewController {
+    
+
+    @IBAction func oneButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func twoButtonPressed(_ sender: UIButton) {
+    }
+    
+ 
+    @IBAction func threeButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func fourButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBOutlet weak var listTitle: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
--- StreamlineApplication/ViewController.swift
+++ StreamlineApplication/ViewController.swift
@@ -8,7 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBAction func importantUrgent_Button(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func importantNotUrgent_Button(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func notImportantNotUrgent_Button(_ sender: UIButton) {
+    }
+    
+    @IBAction func notImportantUrgent_Button(_ sender: UIButton) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
